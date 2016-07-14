e1744b11aaa67b06ee23af2fd6033c545052c630
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-14 17:37:15 -0400

    use isize (can be negative)

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub unsafe extern fn music_note(note: *mut c_char) -> usize {
+pub unsafe extern fn music_note(note: *mut c_char) -> isize {
     match CStr::from_ptr(note).to_str() {
-        Ok(s)  => s.to_string().len(),
+        Ok(s)  => s.to_string().len() as isize,
         Err(_) => 42 // TODO: throw an exception or something
     }
 }
