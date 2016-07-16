bf8b99d4f21d9f9ad28f131fe56c3be065237cd2
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-16 01:52:39 -0400

    Note::from_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 extern crate libc;
 extern crate regex;
 
-use std::ffi::CStr;
-
 mod note;
+
+use note::Note;
+use std::ffi::CStr;
 
 #[no_mangle]
 pub unsafe extern fn music_note(note: *mut libc::c_char) -> isize {
-    match CStr::from_ptr(note).to_str() {
-        Ok(s)  => note::from_str(s),
-        Err(_) => 42 // TODO: throw an exception or something
+    let s = CStr::from_ptr(note).to_str().unwrap_or("");
+    match Note::from_str(s) {
+        Err(_)   => 42, // TODO: throw an exception or something
+        Ok(note) => note.number()
     }
 }
--- src/note.rs
+++ src/note.rs
@@ -1,6 +1,71 @@
 use regex::Regex;
 
-pub fn from_str(s: &str) -> isize {
+pub struct Note {
+    letter: char,
+    // a positive or negative number representing number of semitones up/down
+    accidentals: isize,
+    octave: isize
+}
+
+fn semitones_from_c(letter: char) -> isize {
+    match letter {
+        'c' => 0,
+        'd' => 2,
+        'e' => 4,
+        'f' => 5,
+        'g' => 7,
+        'a' => 9,
+        'b' => 11,
+        _   => panic!(format!("{} isn't a note", letter))
+    }
+}
+
+
+fn parse_note_components(s: &str) -> Result<Note, Option<&str>> {
     let re = Regex::new(r#"([A-Ga-g])([#b]*)(-?\d+)"#).unwrap();
-    s.to_string().len() as isize
+    match re.captures(s) {
+        None => return Err(None),
+        Some(caps) => {
+            let ltr:  char;
+            let mut accs: isize = 0;
+            let oct:  isize;
+
+            match caps.at(1) {
+                None         => { return Err(Some("missing letter")); },
+                Some(letter) => {
+                    ltr = letter.to_lowercase().chars().next().unwrap();
+                }
+            }
+
+            for c in caps.at(2).unwrap().chars() {
+                match c {
+                    'b' => { accs -= 1; },
+                    '#' => { accs += 1; },
+                    _   => { panic!("regex doesn't work lol"); }
+                }
+            }
+
+            match caps.at(3) {
+                None         => { return Err(Some("missing octave")); },
+                Some(octave) => { oct = octave.parse::<isize>().unwrap(); }
+            }
+
+            return Ok(Note { letter: ltr, accidentals: accs, octave: oct });
+        }
+    }
+
+}
+
+impl Note {
+    pub fn from_str(s: &str) -> Result<Note, String> {
+        parse_note_components(s).map_err(|msg| match msg {
+            None      => "Invalid note format".to_string(),
+            Some(msg) => format!("Invalid note format: {} ({})", s, msg)
+        })
+    }
+
+    pub fn number(&self) -> isize {
+        let base_note = semitones_from_c(self.letter) + (self.octave * 12) + 12;
+        base_note + self.accidentals
+    }
 }
