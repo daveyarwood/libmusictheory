f529100a1fad7d6c461b118be3adacdec5d7e16d
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-16 18:58:13 -0400

    panic on error
    Not sure if this is really the right thing to do or not... WIP.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use std::ffi::CStr;
 pub unsafe extern fn musictheory_note(note: *mut libc::c_char) -> isize {
     let s = CStr::from_ptr(note).to_str().unwrap_or("");
     match Note::from_str(s) {
-        Err(_)   => 42, // TODO: throw an exception or something
+        Err(msg) => panic!(msg),
         Ok(note) => note.number()
     }
 }
