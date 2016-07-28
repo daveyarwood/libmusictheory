396c8c17614cb389daf925aefce1bfc13515fe39
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-28 09:44:08 -0400

    more tests

--- ctests/src/ctests.c
+++ ctests/src/ctests.c
@@ -42,7 +42,21 @@ void test_str_equal_impl(void* expected, void* actual, int lineno) {
 bool test_note_number() {
   testing("note_number");
 
+  test_int_equal(0, musictheory_note_number("C-1"));
+  test_int_equal(12, musictheory_note_number("C0"));
+  test_int_equal(24, musictheory_note_number("C1"));
+  test_int_equal(36, musictheory_note_number("C2"));
+  test_int_equal(48, musictheory_note_number("C3"));
+  test_int_equal(60, musictheory_note_number("B#3"));
+  test_int_equal(60, musictheory_note_number("C4"));
   test_int_equal(61, musictheory_note_number("C#4"));
+  test_int_equal(61, musictheory_note_number("Db4"));
+  test_int_equal(62, musictheory_note_number("C##4"));
+  test_int_equal(62, musictheory_note_number("D4"));
+  test_int_equal(62, musictheory_note_number("Ebb4"));
+  test_int_equal(63, musictheory_note_number("Eb4"));
+  test_int_equal(64, musictheory_note_number("E4"));
+  test_int_equal(72, musictheory_note_number("C5"));
 
   return passed;
 }
@@ -50,8 +64,21 @@ bool test_note_number() {
 bool test_spell_note() {
   testing("spell_note");
 
+  test_str_equal("C-1", musictheory_spell_note(0, 'C'));
+  test_str_equal("C0", musictheory_spell_note(12, 'C'));
+  test_str_equal("C1", musictheory_spell_note(24, 'C'));
+  test_str_equal("C2", musictheory_spell_note(36, 'C'));
+  test_str_equal("C3", musictheory_spell_note(48, 'C'));
+  test_str_equal("B#3", musictheory_spell_note(60, 'B'));
+  test_str_equal("C4", musictheory_spell_note(60, 'C'));
   test_str_equal("C#4", musictheory_spell_note(61, 'C'));
   test_str_equal("Db4", musictheory_spell_note(61, 'D'));
+  test_str_equal("C##4", musictheory_spell_note(62, 'C'));
+  test_str_equal("D4", musictheory_spell_note(62, 'D'));
+  test_str_equal("Ebb4", musictheory_spell_note(62, 'E'));
+  test_str_equal("Eb4", musictheory_spell_note(63, 'E'));
+  test_str_equal("E4", musictheory_spell_note(64, 'E'));
+  test_str_equal("C5", musictheory_spell_note(72, 'C'));
 
   return passed;
 }
@@ -59,8 +86,12 @@ bool test_spell_note() {
 int main() {
   bool passed = true;
 
+  printf("\n%s\n\n", "-----");
+
   run_test(test_note_number, "test_note_number", &passed);
   run_test(test_spell_note, "test_spell_note", &passed);
+
+  printf("%s\n\n", "-----");
 
   return passed ? 0 : 1;
 }
